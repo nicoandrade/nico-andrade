ebe62b4812d92f486f0c69fb6bc827392b4e0db8
Nico Andrade <nicoandrade@gmail.com>
2024-01-29 09:20:48 -0300

    self-hosted plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={twMerge("font-sans", catamaran.variable)}>
-                <PlausibleProvider domain={env.NEXT_PUBLIC_SITE_URL.replace("https://", "")}>
+                <PlausibleProvider
+                    domain={env.NEXT_PUBLIC_SITE_URL.replace("https://", "")}
+                    customDomain="https://stats.quemalabs.com"
+                >
                     {children}
                 </PlausibleProvider>
             </body>
